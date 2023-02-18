57f2297de1ec0d9dd1be08a2ff72ecae48c10e32
Carlosfromsubway <tlee285@my.bcit.ca>
2023-02-17 16:35:46 -0800

    stuff

---
+++ comps/Titlelogo.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+import styled from "styled-components";
+import Image from "next/image";
+
+export default function Titlelogo () {
+    return (
+        <>
+        </>
+    )
+}
---
+++ pages/info.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function InfoPage() {
+    return (
+        <>
+        
+        
+        
+        </>
+    )
+}
